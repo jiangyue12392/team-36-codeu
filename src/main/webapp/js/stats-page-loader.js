b5b77bfce98f8d619b2baf941f56b327c08ce5f4
Meg1456 <meghana1456@gmail.com>
2019-06-09 17:58:17 +0800

    made suggested changes to stats-page-loader

--- src/main/webapp/js/stats-page-loader.js
+++ src/main/webapp/js/stats-page-loader.js
@@ -1,27 +1,26 @@
 // Fetch stats and display them in the page.
   function fetchStats() {
     const url = '/stats';
-    fetch(url).then((response) => {
-      return response.json();
-    }).then((stats) => {
+    fetch(url).then((response) => response.json()).then((stats) => {
       const statsContainer = document.getElementById('stats-container');
       statsContainer.innerHTML = '';
 
-      const messageCountElement = buildStatElement('Message count: ' + stats.messageCount);
+      const messageCountElement = buildStatElement(`Message count: ${stats.messageCount}`);
       statsContainer.appendChild(messageCountElement);
 
-      const maxMsgLengthElement = buildStatElement('Maximum message length: ' + stats.maxMsgLength);
+      const maxMsgLengthElement = buildStatElement(`Maximum message length: ${stats.maxMsgLength}`);
       statsContainer.appendChild(maxMsgLengthElement);
     });
   }
 
-    function buildStatElement(statString){
+  // Builds the stat element to be displayed
+  function buildStatElement(statString) {
     const statElement = document.createElement('p');
     statElement.appendChild(document.createTextNode(statString));
     return statElement;
   }
 
   // Fetch data and populate the UI of the page.
-    function buildUI(){
+  function buildUI() {
     fetchStats();
   }
