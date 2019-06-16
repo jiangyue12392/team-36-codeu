b13ff7984fb4597b706a52a7d743495a7d5003c1
Meg1456 <meghana1456@gmail.com>
2019-06-16 17:40:17 +0800

    load and store sentiment score in datastore

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -45,6 +45,7 @@ public class Datastore {
     messageEntity.setProperty("user", message.getUser());
     messageEntity.setProperty("text", message.getText());
     messageEntity.setProperty("timestamp", message.getTimestamp());
+    messageEntity.setProperty("sentimentScore"), message.getSentimentScore());
 
     datastore.put(messageEntity);
   }
@@ -93,8 +94,9 @@ public class Datastore {
         String user = (String) entity.getProperty("user");
         String text = (String) entity.getProperty("text");
         long timestamp = (long) entity.getProperty("timestamp");
+        float sentimentScore = (float) entity.getProperty("sentimentScore");
 
-        Message message = new Message(id, user, text, timestamp);
+        Message message = new Message(id, user, text, timestamp, sentimentScore);
         messages.add(message);
       } catch (Exception e) {
         System.err.println("Error reading message.");
@@ -148,4 +150,6 @@ public class Datastore {
     }
     return maxLength;
   }
+
+
 }
