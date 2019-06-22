9e489171cc69835655bd185804450b9d919c1205
Jiang Yue <jiangyue12392@gmail.com>
2019-06-22 21:06:06 +0800

    add editable markers to map

--- src/main/webapp/js/maps-page-loader.js
+++ src/main/webapp/js/maps-page-loader.js
@@ -7,6 +7,11 @@ const MAP_NIGHT_TYPE_ID = 'night'
  * New modes/map styles can be added in extraMapStyles.json file.
  *
  */
+
+let map;
+/* Editable marker that displays when a user clicks in the map. */
+let editMarker;
+
 function initConfigMap() {
   $.getJSON("/json/extraMapStyles.json", function(json) {
     // Create a new StyledMapType object, passing it an array of styles,
@@ -15,7 +20,7 @@ function initConfigMap() {
 
     // Create a map object, and include the MapTypeId to add
     // to the map type control.
-    const map = new google.maps.Map(document.getElementById(MAP_ELEMENT_ID), {
+    map = new google.maps.Map(document.getElementById(MAP_ELEMENT_ID), {
       center: {lat: 1.2764558, lng: 103.7996469},
       zoom: 16,
       mapTypeControlOptions: {
@@ -39,5 +44,85 @@ function initConfigMap() {
     // Associate the styled map with the MapTypeId and set it to display.
     map.mapTypes.set(MAP_NIGHT_TYPE_ID, styledMapType);
     map.setMapTypeId(MAP_NIGHT_TYPE_ID);
+
+    // When the user clicks in the map, show a marker with a text box the user can edit.
+    map.addListener('click', (event) => {
+      createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+    });
+    fetchMarkers();
   });
+}
+
+/** Fetches markers from the backend and adds them to the map. */
+function fetchMarkers(){
+  fetch('/markers').then((response) => {
+    return response.json();
+  }).then((markers) => {
+    markers.forEach((marker) => {
+     createMarkerForDisplay(marker.lat, marker.lng, marker.content)
+    });
+  });
+}
+
+/** Creates a marker that shows a read-only info window when clicked. */
+function createMarkerForDisplay(lat, lng, content){
+  const marker = new google.maps.Marker({
+    position: {lat: lat, lng: lng},
+    map: map
+  });
+  var infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+  marker.addListener('click', () => {
+    infoWindow.open(map, marker);
+  });
+}
+
+/** Sends a marker to the backend for saving. */
+function postMarker(lat, lng, content){
+  const params = new URLSearchParams();
+  params.append('lat', lat);
+  params.append('lng', lng);
+  params.append('content', content);
+  fetch('/markers', {
+    method: 'POST',
+    body: params
+  });
+}
+
+/** Creates a marker that shows a textbox the user can edit. */
+function createMarkerForEdit(lat, lng){
+  // If an editable marker is already shown, remove it.
+  if (editMarker) {
+   editMarker.setMap(null);
+  }
+  editMarker = new google.maps.Marker({
+    position: {lat: lat, lng: lng},
+    map: map
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: buildInfoWindowInput(lat, lng)
+  });
+  // Removes the marker when the user closes the editable info window.
+  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+    editMarker.setMap(null);
+  });
+  infoWindow.open(map, editMarker);
+}
+
+/** Builds and returns HTML elements that show an editable textbox and a submit button. */
+function buildInfoWindowInput(lat, lng){
+  const textBox = document.createElement('textarea');
+  const button = document.createElement('button');
+  button.appendChild(document.createTextNode('Submit'));
+  button.onclick = () => {
+    postMarker(lat, lng, textBox.value);
+    createMarkerForDisplay(lat, lng, textBox.value);
+    editMarker.setMap(null);
+  };
+  const containerDiv = document.createElement('div');
+  containerDiv.appendChild(textBox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(button);
+  return containerDiv;
 }
